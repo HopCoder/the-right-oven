e231daaa33f58302f0b6445f1ce7ef45d235647e
Jonathan Go <jonathanjgo@gmail.com>
2015-02-20 23:49:47 -0500

    Mock splash screen with color and placeholder icon.

--- Codename_Puck/app/src/main/java/com/blogspot/therightoveninc/codenamepuck/main_menu.java
+++ Codename_Puck/app/src/main/java/com/blogspot/therightoveninc/codenamepuck/main_menu.java
@@ -11,7 +11,7 @@ public class main_menu extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_menu);
+        setContentView(R.layout.intro);
     }
 
 
