e90d0bfc1a41ede4efcb117d0bfc3b67993b9250
Hopper <mrfredericks@gmail.com>
2015-03-05 21:21:39 +0000

    Added a new module for pictures, and synced with MySQL using Python3

--- puck_passer/puck_passer/settings.py
+++ puck_passer/puck_passer/settings.py
@@ -57,9 +57,17 @@ WSGI_APPLICATION = 'puck_passer.wsgi.application'
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+    'default':{
+        'ENGINE':'django.db.backends.mysql',
+        'NAME':'puck',
+        'USER':'django',
+        'PASSWORD':'ognajd',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
