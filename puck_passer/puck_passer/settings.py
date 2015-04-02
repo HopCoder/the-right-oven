124a0c39751cc0f39a6f34a84428daccceac7f75
Hopper <mrfredericks@gmail.com>
2015-04-02 20:00:47 +0000

    Accidentally uncommented a comment.  Its fixed now

--- puck_passer/puck_passer/settings.py
+++ puck_passer/puck_passer/settings.py
@@ -59,12 +59,12 @@ WSGI_APPLICATION = 'puck_passer.wsgi.application'
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
-# SQLlite
+# SQLlite - Always keep this line commented
 #    'default': {
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #    }
- MySQL
+# MySQL - Always keep this line commented
     'default':{
         'ENGINE':'django.db.backends.mysql',
         'NAME':'puck',
