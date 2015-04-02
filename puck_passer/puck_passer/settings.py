18354ff71561609eb72d2ac450f8e904651a97e8
Hopper <mrfredericks@gmail.com>
2015-04-02 11:38:42 -0400

    Merged from Hopper. Verification needed for db
    This needs to be put on AWS server and checked to make sure I didn't
    break the MySQL database.  It should be good (I think).  After
    verification, it can be merged up to master, and then all the django
    code for initial GET/POST methods should be complete, and ready for
    integration, and future iterations.

--- puck_passer/puck_passer/settings.py
+++ puck_passer/puck_passer/settings.py
@@ -60,19 +60,19 @@ WSGI_APPLICATION = 'puck_passer.wsgi.application'
 
 DATABASES = {
 # SQLlite
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-# MySQL
-#    'default':{
-#        'ENGINE':'django.db.backends.mysql',
-#        'NAME':'puck',
-#        'USER':'django',
-#        'PASSWORD':'ognajd',
-#        'HOST':'localhost',
-#        'PORT':'3306',
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #    }
+ MySQL
+    'default':{
+        'ENGINE':'django.db.backends.mysql',
+        'NAME':'puck',
+        'USER':'django',
+        'PASSWORD':'ognajd',
+        'HOST':'localhost',
+        'PORT':'3306',
+    }
 }
 
 # Internationalization
