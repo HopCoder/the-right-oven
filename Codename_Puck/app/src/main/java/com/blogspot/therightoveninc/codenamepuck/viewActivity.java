ff9673e739b63a739b0a068fa15275f5d9a89d2e
Oranga <tangdtd@gmail.com>
2015-04-15 23:42:47 -0400

    Merged with Gromit for more server Functionality

--- Codename_Puck/app/src/main/java/com/blogspot/therightoveninc/codenamepuck/viewActivity.java
+++ Codename_Puck/app/src/main/java/com/blogspot/therightoveninc/codenamepuck/viewActivity.java
@@ -1,32 +1,25 @@
 package com.blogspot.therightoveninc.codenamepuck;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
-import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.PopupWindow;
-import android.widget.RelativeLayout;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 
-public class receive extends ActionBarActivity {
+public class viewActivity extends Activity {
     public int messageCount = 3;
     private PopupWindow popupWindow;
 
@@ -34,9 +27,7 @@ public class receive extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.receive);
-
-        setDimensions();
+        setContentView(R.layout.view);
 
         if (messageCount > 0)
         {
@@ -55,8 +46,8 @@ public class receive extends ActionBarActivity {
 
             try
             {
-                //url = new URL("http://52.10.111.12:8000/static/a.jpg");
-                url = new URL("http://upsilondelts.org/images/bros/thaw.jpg");
+                url = new URL("http://52.10.111.12:8000/view/1234567890/69/34/50");
+//                url = new URL("http://upsilondelts.org/images/bros/thaw.jpg");
                 image = BitmapFactory.decodeStream(url.openConnection().getInputStream());
              //   i.setImageBitmap(image);
             }
@@ -81,36 +72,6 @@ public class receive extends ActionBarActivity {
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        switch (id)
-        {
-            case R.id.action_settings:
-
-                return true;
-            case R.id.action_report:
-                reportClick();
-                return true;
-            case R.id.action_camera:
-                onCameraClick();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
     private void getImage()
     {
         ImageView i = (ImageView) findViewById(R.id.imageView);
@@ -131,61 +92,15 @@ public class receive extends ActionBarActivity {
         }
     }
 
-    private void setDimensions()
+    public void commentClick(View v)
     {
-        // Get Action Bar Height (pixels)
-        TypedValue tv = new TypedValue();
-        this.getTheme().resolveAttribute(android.R.attr.actionBarSize, tv, true);
-        int actionBarHeight = getResources().getDimensionPixelSize(tv.resourceId);
+        return;
+    }
 
-        // Convert pixels to dp
-        float fl_width = math.convertPixelsToDp(phoneSettings.xPixels);
-        float fl_height = math.convertPixelsToDp(phoneSettings.yPixels);
-        float fl_actionBarHeight = math.convertPixelsToDp(actionBarHeight);
-
-        // Adjust dimensions to have 15 dp margins
-        fl_width -= 30f;
-
-        // Calculate dp values
-        float fl_button = (fl_height - (fl_width + 15f + fl_actionBarHeight )) / 2;
-        float fl_button_margin = (float) (fl_width - (2.5 * fl_button)) / 2;
-        float fl_spacer_width = fl_button / 2;
-
-        // Convert dp to pixels
-        int pic_size = (int)math.convertDpToPixel(fl_width);
-        int button_size = (int)math.convertDpToPixel(fl_button);
-        int button_margin = (int)math.convertDpToPixel(fl_button_margin);
-        int spacer_width = (int)math.convertDpToPixel(fl_spacer_width);
-
-        // Set picture dimensions
-        ImageView image = (ImageView) findViewById(R.id.imageView);
-        ViewGroup.LayoutParams imageParams = image.getLayoutParams();
-        imageParams.width = pic_size;
-        imageParams.height = pic_size;
-        image.setLayoutParams(imageParams);
-
-        // Set button dimensions
-        ImageButton shuck_button = (ImageButton) findViewById(R.id.shuck_button);
-        Button puck_button = (Button) findViewById(R.id.puck_button);
-        ViewGroup.LayoutParams buttonParams = shuck_button.getLayoutParams();
-        buttonParams.width = button_size;
-        buttonParams.height = button_size;
-        shuck_button.setLayoutParams(buttonParams);
-        puck_button.setLayoutParams(buttonParams);
-
-        // Set spacer width
-        View view = findViewById(R.id.view);
-        ViewGroup.LayoutParams spacerParams = view.getLayoutParams();
-        spacerParams.width = spacer_width;
-        spacerParams.height = 1;
-        view.setLayoutParams(spacerParams);
-
-        // Set button spacing and locations
-        View linearLayout = findViewById(R.id.linear_layout);
-        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        params.setMargins(button_margin,spacerParams.width/2,0,0);
-        params.addRule(RelativeLayout.BELOW, R.id.imageView);
-        linearLayout.setLayoutParams(params);
+    public void cameraClick(View v)
+    {
+        Intent cameraIntent = new Intent(this, cameraActivity.class);
+        startActivity(cameraIntent);
     }
 
     public void puckClick(View v)
@@ -209,7 +124,7 @@ public class receive extends ActionBarActivity {
         LayoutInflater layoutInflater
                 = (LayoutInflater)getBaseContext()
                 .getSystemService(LAYOUT_INFLATER_SERVICE);
-        View popupView = layoutInflater.inflate(R.layout.popup, null);
+        View popupView = layoutInflater.inflate(R.layout.report, null);
         popupWindow = new PopupWindow(
                 popupView, LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT, false);
         popupWindow.setOutsideTouchable(false);
@@ -228,11 +143,6 @@ public class receive extends ActionBarActivity {
     public void reportCancelClick(View v)
     {
         popupWindow.dismiss();
-    }
-
-    public void onCameraClick(){
-        Intent cameraIntent = new Intent(this, cameraActivity.class);
-        startActivity(cameraIntent);
     }
 
     private void messageDelete()
